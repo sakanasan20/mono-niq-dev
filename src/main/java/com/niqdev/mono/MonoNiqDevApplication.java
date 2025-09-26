d4ea380da15e6714465a9810b34b6905b5fb6089
Nick Chen <nick.chen@cmp.com.tw>
2025-09-26 08:33:15 +0800

    Initial commit

---
+++ src/main/java/com/niqdev/mono/MonoNiqDevApplication.java
@@ -0,0 +1,13 @@
+package com.niqdev.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoNiqDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoNiqDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niqdev/mono/MonoNiqDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niqdev.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoNiqDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
